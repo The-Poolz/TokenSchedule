8b06b759a6bfc12d07400d4288f6d1353a81d8a9
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-04-20 14:23:26 +0300

    - import code from `TokenScheduleModel` repository - use `netstandard2.1`

---
+++ src/TokenSchedule/SingleRow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TokenSchedule
+{
+    public class SingleRow
+    {
+        public SingleRow(decimal ratio, DateTime startTime, DateTime? endTime = null)
+        {
+            Ratio = ratio;
+            StartTime = startTime;
+            EndTime = endTime;
+
+            if (EndTime.HasValue && StartTime >= EndTime.Value)
+            {
+                throw new ArgumentException("End time must be greater than start time.", nameof(startTime));
+            }
+            if (Ratio <= 0)
+            {
+                throw new ArgumentException("Ratio must be positive.", nameof(ratio));
+            }
+        }
+
+        public decimal Ratio { get; }
+        public DateTime StartTime { get; }
+        public DateTime? EndTime { get; }
+    }
+}
