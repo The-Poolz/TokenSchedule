6c1323db1fa7842a012be2450dcef84b3256fd8f
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-04-22 13:07:14 +0300

    - add `ScheduleItemTests`

---
+++ tests/TokenSchedule.Tests/ScheduleItemTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace TokenSchedule.Tests;
+
+public class ScheduleItemTests
+{
+    public class Constructor
+    {
+        [Fact]
+        internal void WhenFinishTimeLowerThenStartTime_ThrowException()
+        {
+            var testCode = () => _ = new ScheduleItem(0.1m, DateTime.Now, DateTime.Now.AddDays(-1));
+
+            testCode.Should().Throw<ArgumentException>()
+                .WithMessage("End time must be greater than start time. (Parameter 'startTime')");
+        }
+
+        [Fact]
+        internal void WhenRatioIsNegative_ThrowException()
+        {
+            var testCode = () => _ = new ScheduleItem(-0.1m, DateTime.Now, DateTime.Now.AddDays(1));
+
+            testCode.Should().Throw<ArgumentException>()
+                .WithMessage("Ratio must be positive. (Parameter 'ratio')");
+        }
+    }
+}
