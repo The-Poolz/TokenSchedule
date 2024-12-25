4f1adb22515c08d35909d3b2755d25e34012714f
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-12-25 13:36:17 +0300

    - initial create `TokenSchedule.FluentValidation` project

---
+++ src/TokenSchedule.FluentValidation/ScheduleItemValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using TokenSchedule.FluentValidation.Models;
+
+namespace TokenSchedule.FluentValidation
+{
+    public class ScheduleItemValidator : AbstractValidator<IValidatedScheduleItem>
+    {
+        public ScheduleItemValidator()
+        {
+            RuleFor(item => item)
+                .Must(item => item.Ratio > 0)
+                .WithMessage("Ratio must be positive.")
+                .Must(item => !item.FinishDate.HasValue || item.StartDate < item.FinishDate.Value)
+                .WithMessage("End time must be greater than start time.");
+        }
+    }
+}
---
+++ src/TokenSchedule.FluentValidation/ScheduleValidator.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using FluentValidation;
+using System.Collections.Generic;
+using TokenSchedule.FluentValidation.Models;
+
+namespace TokenSchedule.FluentValidation
+{
+    public class ScheduleValidator : AbstractValidator<IEnumerable<IValidatedScheduleItem>>
+    {
+        public ScheduleValidator()
+        {
+            RuleFor(schedule => schedule.ToArray())
+                .NotEmpty()
+                .WithMessage("Schedule must contain 1 or more elements.")
+                .Must(schedule => schedule.Sum(item => item.Ratio) != 1.0m)
+                .WithMessage("The sum of the ratios must be 1.")
+                .Must(schedule => schedule[0].StartDate != schedule.Min(x => x.StartDate))
+                .WithMessage("The first element must be the TGE (Token Generation Event).")
+                .ForEach(item => item.SetValidator(new ScheduleItemValidator()));
+        }
+    }
+}
