fc7b7880733f625919628da098a3bb162fba087c
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-12-25 14:55:49 +0300

    - fix exist tests - write validator tests

---
+++ tests/TokenSchedule.FluentValidation.Tests/TestScheduleItem.cs
@@ -0,0 +1,17 @@
+﻿using TokenSchedule.FluentValidation.Models;
+
+namespace TokenSchedule.FluentValidation.Tests;
+
+public class TestScheduleItem : IValidatedScheduleItem
+{
+    public TestScheduleItem(decimal ratio, DateTime startDate, DateTime? finishDate)
+    {
+        Ratio = ratio;
+        StartDate = startDate;
+        FinishDate = finishDate;
+    }
+
+    public decimal Ratio { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime? FinishDate { get; set; }
+}
